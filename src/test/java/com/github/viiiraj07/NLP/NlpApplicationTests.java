7c1e450858150671855a331f16786dfe0a7bd605
viiiraj07 <avisec07@gmail.com>
2023-07-01 22:53:48 +0530

    project init

---
+++ src/test/java/com/github/viiiraj07/NLP/NlpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viiiraj07.NLP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
