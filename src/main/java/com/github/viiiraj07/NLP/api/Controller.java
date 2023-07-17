cb632e3b483cd7e3b1efbfd3c90f288650fc2d0d
abhishek-rabidas <avisec07@gmail.com>
2023-07-17 22:29:42 +0530

    model integrate

--- src/main/java/com/github/viiiraj07/NLP/api/Controller.java
+++ src/main/java/com/github/viiiraj07/NLP/api/Controller.java
@@ -1,11 +1,25 @@
 package com.github.viiiraj07.NLP.api;
 
+import com.github.viiiraj07.NLP.engine.TokenizerEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/")
 @RestController
 public class Controller {
+
+    @Autowired
+    private TokenizerEngine tokenizerEngine;
+
+    @PostMapping("/tokenize")
+    public ResponseEntity<?> TokenizeString(@RequestParam("text") String text) {
+
+        return ResponseEntity.ok(tokenizerEngine.StringTokenize(text));
+    }
 
 
 }
--- src/main/java/com/github/viiiraj07/NLP/engine/TokenizerEngine.java
+++ src/main/java/com/github/viiiraj07/NLP/engine/TokenizerEngine.java
@@ -1,8 +1,27 @@
 package com.github.viiiraj07.NLP.engine;
 
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class TokenizerEngine {
+
+    public String[] StringTokenize(String text) {
+
+        try (InputStream modelIn = new FileInputStream("src/main/resources/models/en-token.bin")) {
+            TokenizerModel model = new TokenizerModel(modelIn);
+            Tokenizer tokenizer = new TokenizerME(model);
+            return tokenizer.tokenize(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
