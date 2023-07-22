855dd9ec204d0ebb1d1403555bd3d9a7bbbf3c58
abhishek-rabidas <avisec07@gmail.com>
2023-07-22 18:47:11 +0530

    language detection service logic

--- src/main/java/com/github/viiiraj07/NLP/engine/LanguageDetector.java
+++ src/main/java/com/github/viiiraj07/NLP/engine/LanguageDetector.java
@@ -1,5 +1,8 @@
 package com.github.viiiraj07.NLP.engine;
 
+import opennlp.tools.langdetect.Language;
+import opennlp.tools.langdetect.LanguageDetectorME;
+import opennlp.tools.langdetect.LanguageDetectorModel;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
@@ -10,9 +13,12 @@ import java.io.InputStream;
 @Service
 public class LanguageDetector {
 
-    public String detectLanguage() {
-        try (InputStream modelIn = new FileInputStream("src/main/resources/models/en-ner-person.bin")) {
-
+    public String detectLanguage(String inputText) {
+        try (InputStream modelIn = new FileInputStream("src/main/resources/models/langdetect-183.bin")) {
+            LanguageDetectorModel m = new LanguageDetectorModel(modelIn);
+            LanguageDetectorME myCategorize = new LanguageDetectorME(m);
+            Language bestLanguage = myCategorize.predictLanguage(inputText);
+            return bestLanguage.getLang();
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
