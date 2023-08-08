dd38a3715bc13534a807cb480ead22f914c3a177
abhishek-rabidas <avisec07@gmail.com>
2023-08-08 18:21:22 +0530

    version check post contruct

--- src/main/java/com/github/viiiraj07/NLP/util/VersionCheck.java
+++ src/main/java/com/github/viiiraj07/NLP/util/VersionCheck.java
@@ -1,4 +1,19 @@
 package com.github.viiiraj07.NLP.util;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class VersionCheck {
+
+    @Value("${nlp-app-version}")
+    private String ver;
+
+    @PostConstruct
+    public String getVersion() {
+        System.out.println("Version: " + ver);
+        return ver;
+    }
+
 }
