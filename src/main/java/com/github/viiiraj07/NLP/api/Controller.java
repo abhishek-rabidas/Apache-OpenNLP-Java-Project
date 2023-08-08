e17263fc3766f614bdf88d16ac38c3fcd6636ce2
abhishek-rabidas <avisec07@gmail.com>
2023-08-08 17:59:15 +0530

    cors origin

--- src/main/java/com/github/viiiraj07/NLP/api/Controller.java
+++ src/main/java/com/github/viiiraj07/NLP/api/Controller.java
@@ -5,13 +5,11 @@ import com.github.viiiraj07.NLP.engine.NamingEngine;
 import com.github.viiiraj07.NLP.engine.TokenizerEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/")
 @RestController
+@CrossOrigin
 public class Controller {
 
     @Autowired
